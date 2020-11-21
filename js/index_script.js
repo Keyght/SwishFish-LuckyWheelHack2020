4ec118d08501203b9e3167b08075e8a3dd8c23ec
Marina <mbakanova2016@yandex.ru>
2020-11-21 17:41:26 +0300

    added geolocation api

--- js/index_script.js
+++ js/index_script.js
@@ -10,6 +10,36 @@ const ro = new ResizeObserver((entries) => {
 	}
 });
 
+//Geolocation API
+const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.innerHTML = `Latitude: ${latitude} °`+"<br>"+`Longitude: ${longitude} °`;
+  }
+
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if(!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+  
+//End of Geolocation API
+
+
 ro.observe(document.querySelector("div"));
 
 //#region  Web Storage API
