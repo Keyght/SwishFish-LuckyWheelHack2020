970ccdc580c9b4b33933343a09168e030e9b10a7
Dmitriy <Dimamakarov1941@yandex.ru>
2020-11-21 12:27:49 +0300

    style: :lipstick: game js

--- js/flippyGame.js
+++ js/flippyGame.js
@@ -1,88 +1,124 @@
 function getRandomInt(e) {
 	return Math.floor(Math.random() * Math.floor(e));
 }
+
 var map = getRandomInt(3);
+
 let cvs = document.getElementById("canvas"),
 	ctx = cvs.getContext("2d"),
 	body_tag = document.querySelector("body"),
 	canvas_tag = document.querySelector("canvas");
+
 (body_tag.style.width = "100%"),
 	(body_tag.style.height = "100%"),
 	(body_tag.style.minHeight = "512px"),
 	(canvas_tag.style.width = "100%"),
 	(canvas_tag.style.height = "100%");
+
+// host = "https://more02.github.io/Lucky_wheel_hack2020/";
+
 let bird = new Image(),
 	bg = new Image(),
 	fg = new Image(),
 	pipeUp = new Image(),
 	pipeBottom = new Image();
-var protocol = location.protocol,
-	slashes = protocol.concat("//"),
-	//host = slashes.concat(window.location.hostname);
-//host += ":5501/";
-host = "https://more02.github.io/Lucky_wheel_hack2020/";
-(bird.src = host + "images/fish/fish" + map + ".png"),
-	(bg.src = host + "images/water/water" + map + ".png"),
-	(pipeUp.src = host + "images/pipeUp.png"),
-	(pipeBottom.src =  host+"images/pipeBottom.png"),
+
+(bird.src = "images/fish/fish" + map + ".png"),
+	(bg.src = "images/water/water" + map + ".png"),
+	(pipeUp.src = "images/pipeUp.png"),
+	(pipeBottom.src = "images/pipeBottom.png"),
 	(bird.style.width = "38px"),
 	(bird.style.length = "26px"),
 	(bg.style.width = "100%"),
 	(bg.style.height = "100%");
-let fly = new Audio(),
-	score_audio = new Audio();
-(fly.src = host + "sounds/fly.mp3"), (score_audio.src = "sounds/score.mp3");
-let gap = 120;
+
+// some variables
+
+var gap = 85;
+var constant;
+
+var bX = 10;
+var bY = 150;
+
+var gravity = 1.5;
+
+var score = 0;
+
+// audio files
+
+var fly = new Audio();
+var scor = new Audio();
+
+fly.src = "sounds/fly.mp3";
+scor.src = "sounds/score.mp3";
+
+// on key down
+
+document.addEventListener("keydown", moveUp);
+
 function moveUp() {
-	(yPos -= 35), fly.play();
+	bY -= 25;
+	fly.play();
 }
-function TouchStart(e) {
-	(yPos -= 35), fly.play();
-}
-document.addEventListener("touchstart", function (e) {
-	TouchStart(e);
-}),
-	document.addEventListener("keydown", moveUp);
-let pipe = [];
-pipe[0] = { x: cvs.width, y: 0 };
-let score = 0,
-	score2=0,
-	xPos = 10,
-	yPos = 150,
-	grav = 1.5,
-	q=0;
+
+// pipe coordinates
+
+var pipe = [];
+
+pipe[0] = {
+	x: cvs.width,
+	y: 0,
+};
+
 function draw() {
 	ctx.drawImage(bg, 0, 0);
-	for (let e = 0; e < pipe.length; e++) {
-		ctx.drawImage(pipeUp, pipe[e].x, pipe[e].y),
-			ctx.drawImage(pipeBottom, pipe[e].x, pipe[e].y + pipeUp.height + gap),
-				
-			pipe[e].x--,
-			125 == pipe[e].x &&
-				pipe.push({
-					x: cvs.width,
-					y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height,
-				}),
-			((xPos + bird.width >= pipe[e].x &&
-				xPos <= pipe[e].x + pipeUp.width &&
-				(yPos <= pipe[e].y + pipeUp.height ||
-					yPos + bird.height >= pipe[e].y + pipeUp.height + gap)) ||
-				yPos + bird.height >= cvs.height - fg.height) &&
-				location.reload();
-			if(pipe[e].x == 5) {
-				score++;
-				score_audio.play();
-			}
-			
-	}
-	ctx.drawImage(fg, 0, cvs.height - fg.height),
-		ctx.drawImage(bird, xPos, yPos),
-		(yPos += grav),
-		(ctx.fillStyle = "#FFFFFF"),
-		(ctx.font = "24px Arial"),
-		ctx.fillText("Счет: " + score, 10, cvs.height - 20),
-		requestAnimationFrame(draw);
 
-	
+	for (var i = 0; i < pipe.length; i++) {
+		constant = pipeUp.height + gap;
+		ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+		ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + constant);
+
+		pipe[i].x--;
+
+		if (pipe[i].x == 125) {
+			pipe.push({
+				x: cvs.width,
+				y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height,
+			});
+		}
+
+		// detect collision
+
+		if (
+			(bX + bird.width >= pipe[i].x &&
+				bX <= pipe[i].x + pipeUp.width &&
+				(bY <= pipe[i].y + pipeUp.height ||
+					bY + bird.height >= pipe[i].y + constant)) ||
+			bY + bird.height >= cvs.height - fg.height
+		) {
+			location.reload(); // reload the page
+		}
+
+		if (pipe[i].x == 5) {
+			score++;
+			scor.play();
+		}
+	}
+	ctx.drawImage(fg, 0, cvs.height - fg.height);
+
+	ctx.drawImage(bird, bX, bY);
+
+	bY += gravity;
+
+	ctx.fillStyle = "#000";
+	ctx.font = "20px Verdana";
+	ctx.fillText("Score : " + score, 10, cvs.height - 20);
+
+	// if (yPos > 500) {
+	// 	console.log("yPos" + yPos);
+
+	// 	location.reload();
+	// }
+	requestAnimationFrame(draw);
 }
 pipeBottom.onload = draw;
