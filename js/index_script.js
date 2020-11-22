d39455f4af60a9ac6719a8a24e547c6d49dbd6aa
Marina <mbakanova2016@yandex.ru>
2020-11-22 13:30:29 +0300

    check button5

--- js/index_script.js
+++ js/index_script.js
@@ -87,10 +87,10 @@ if (typeof Storage !== "undefined") {
 function WhatMap(map) {
 	console.log("map " + map);
 	var url = document.location.host;
-	// host = "https://more02.github.io/Lucky_wheel_hack2020/";
-	if (map == 0) window.location.href = "https://more02.github.io/Lucky_wheel_hack2020/Game.html";
-	else if (map == 1) window.location.href = "https://more02.github.io/Lucky_wheel_hack2020/GameVoice.html";
-	else if (map == 2) window.location.href = "https://more02.github.io/Lucky_wheel_hack2020/GameVoice.html";
+	let host = "https://more02.github.io/Lucky_wheel_hack2020/";
+	if (map == 0) window.location.href = host+"Game.html";
+	else if (map == 1) window.location.href = host+"GameVoice.html";
+	else if (map == 2) window.location.href = host+"GameVoice.html";
 	localStorage.setItem("map", toString(map));
 }
 function changeName(name) {
